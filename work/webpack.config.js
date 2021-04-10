04d3c68fee0bab2f71666178b68325cec6dd156c
nakagawa <nakagawa@brave-heart.co.jp>
2021-04-10 12:17:09 +0900

    initial commit

---
+++ work/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'public/js')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [['@babel/preset-env',{modules:false}]]
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
